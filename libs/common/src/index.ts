e64edc7b475c1a906744d1e2454a0365203be903
hieple <hieplevuc@gmail.com>
2024-04-30 14:44:01 +0700

    feat: Add email dtos + queue common

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,3 +2,5 @@ export * from './gateway-middleware';
 export * from './helper';
 export * from './logger';
 export * from './interfaces';
+export * from './dtos';
+export * from './queue';
---
+++ libs/common/src/queue/index.ts
@@ -0,0 +1,5 @@
+export const AUTH_EMAIL_QUEUE_NAME = 'auth_email_queue';
+export const ORDER_EMAIL_QUEUE_NAME = 'order_email_queue';
+export const EVENTS_RMQ = {
+  AUTH_EMAIL: 'auth_email_event',
+};
