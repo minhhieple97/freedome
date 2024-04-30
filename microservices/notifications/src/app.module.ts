7bf20139dc5d31b2d97a0db6948cb6874a22060c
hieple <hieplevuc@gmail.com>
2024-04-30 14:45:22 +0700

    refactor: Move Email module into App Module

--- microservices/notifications/src/app.module.ts
+++ microservices/notifications/src/app.module.ts
@@ -4,16 +4,19 @@ import { HealthController } from './api/health/health.controller';
 import { ElasticsearchModule } from './config/elasticsearch/elasticsearch.module';
 import { WinstonModule } from 'nest-winston';
 import { AppService } from './app.service';
+import { EmailModule } from './consumers/email/email.module';
+import { AppController } from './app.controller';
 @Module({
   imports: [
     AppConfigModule,
     ElasticsearchModule,
+    EmailModule,
     WinstonModule.forRootAsync({
       useFactory: () => ({}),
       inject: [],
     }),
   ],
-  controllers: [HealthController],
+  controllers: [HealthController, AppController],
   providers: [AppService],
   exports: [AppService],
 })
