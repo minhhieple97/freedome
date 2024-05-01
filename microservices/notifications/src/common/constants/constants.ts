4fe0c4ce5157fcf9d7a8bceceb13bcddd8e0469a
hieple <hieplevuc@gmail.com>
2024-05-01 07:26:27 +0700

    feat: Add constants for email templates name

--- microservices/notifications/src/common/constants/constants.ts
+++ microservices/notifications/src/common/constants/constants.ts
@@ -1,2 +1,13 @@
 export const ENVIRONMENT_PATH = 'src/common/environments';
 export const DEFAULT_PORT = 3001;
+export const EMAIL_TEMPLATES_NAME = {
+  FORGOT_PASSWORD: 'forgot-password',
+  OFFER: 'offer',
+  ORDER_DELIVERED: 'order-delivered',
+  ORDER_EXTENSION_APPROVAL: 'order-extension-approval',
+  ORDER_EXTENSION: 'order-extension',
+  ORDER_PLACED: 'order-placed',
+  ORDER_RECEIPT: 'order-receipt',
+  RESET_PASSWORD_SUCCESS: 'reset-password-success',
+  VERIFY_EMAIL: 'verify-email',
+};
