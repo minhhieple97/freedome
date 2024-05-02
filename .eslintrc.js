d934daabf60f0553bfaccc3166174c22cfa6f0ca
hieple <hieplevuc@gmail.com>
2024-05-02 14:25:08 +0700

    fix: Fix bug Parsing error: ESLint was configured to run on `<tsconfigRootDir>/package.json`

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    extraFileExtensions: [".json"]
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
