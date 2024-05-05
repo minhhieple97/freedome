eec56ae848c71265507c6b64d83daf9b33349db0
hieple <hieplevuc@gmail.com>
2024-05-05 07:05:06 +0700

    feat: Setup middleware with helmet and compression

--- microservices/gateway/src/main.ts
+++ microservices/gateway/src/main.ts
@@ -4,20 +4,33 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { GatewayModule } from './gateway.module';
 import { LoggerService } from '@freedome/common';
 import { AppConfigService } from './config/app/config.service';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import * as compression from 'compression';
 const logger = new LoggerService('APIGW Service');
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(GatewayModule, {
     cors: true,
     logger,
   });
-  const options = new DocumentBuilder()
-    .setTitle('API docs')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
-  app.setGlobalPrefix('api/gateway/v1');
   const appConfig: AppConfigService = app.get(AppConfigService);
+  app.setGlobalPrefix('api/gateway/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.use(compression());
+  app.use(helmet());
+  if (appConfig.nodeEnv == 'development') {
+    const options = new DocumentBuilder()
+      .setTitle('API docs')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
   await app.listen(appConfig.appPort);
 }
 bootstrap();
