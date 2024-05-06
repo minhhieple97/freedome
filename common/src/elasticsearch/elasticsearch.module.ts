6493a1f664ee593b6a4942360a5fd4c616667ba8
hieple <hieplevuc@gmail.com>
2024-05-06 08:57:49 +0700

    feat: Move elasticsearch module to common module

--- common/src/elasticsearch/elasticsearch.module.ts
+++ common/src/elasticsearch/elasticsearch.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ElasticsearchModule as ElasticSearchModule } from '@nestjs/elasticsearch';
-import { AppConfigModule } from '../app/config.module';
-import { AppConfigService } from '../app/config.service';
 import { ElasticsearchService } from './elasticsearch.service';
 
 @Module({
   imports: [
     ElasticSearchModule.registerAsync({
-      imports: [AppConfigModule],
-      useFactory: async (appConfigService: AppConfigService) => ({
-        node: appConfigService.elasticSearchUrl,
+      imports: [],
+      useFactory: async () => ({
+        node: process.env.ELASTIC_SEARCH_URL,
       }),
-      inject: [AppConfigService],
+      inject: [],
     }),
   ],
   providers: [ElasticsearchService],
