fb2261e7c8ba6f177e05a15398e5eebc55493288
hieple <hieplevuc@gmail.com>
2024-05-08 22:11:59 +0700

    feat: Add constants for event http

--- common/src/queue/index.ts
+++ common/src/queue/index.ts
@@ -3,3 +3,6 @@ export const ORDER_EMAIL_QUEUE_NAME = 'order_email_queue';
 export const EVENTS_RMQ = {
   AUTH_EMAIL: 'auth_email_event',
 };
+export const EVENTS_HTTP = {
+  USER_CREATE: 'user_create',
+};
