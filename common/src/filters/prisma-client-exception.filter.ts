551a24e406be1d60cf73b727d6605fd2663d8077
hieple <hieplevuc@gmail.com>
2024-05-08 22:16:10 +0700

    feat: Add prisma client handler error

---
+++ common/src/filters/prisma-client-exception.filter.ts
@@ -0,0 +1,38 @@
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+import { PrismaError } from '../enums/prisma-error.enum';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    console.error(exception.message);
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const message = exception.message.replace(/\n/g, '');
+
+    switch (exception.code) {
+      case PrismaError.UniqueConstraintFailed: {
+        const status = HttpStatus.CONFLICT;
+        response.status(status).json({
+          statusCode: status,
+          message: message,
+        });
+        break;
+      }
+      case PrismaError.RecordDoesNotExist: {
+        const status = HttpStatus.NOT_FOUND;
+        response.status(status).json({
+          statusCode: status,
+          message: message,
+        });
+        break;
+      }
+      default:
+        // default 500 error code
+        super.catch(exception, host);
+        break;
+    }
+  }
+}
