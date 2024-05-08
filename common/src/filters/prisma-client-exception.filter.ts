7681f13c1e408216a2ad6e70a9a66feeae6cb48d
hieple <hieplevuc@gmail.com>
2024-05-08 22:19:46 +0700

    refactor: Refactor prisma client handle error

--- common/src/filters/prisma-client-exception.filter.ts
+++ common/src/filters/prisma-client-exception.filter.ts
@@ -7,7 +7,6 @@ import { PrismaError } from '../enums/prisma-error.enum';
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
-    console.error(exception.message);
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const message = exception.message.replace(/\n/g, '');
--- common/src/prisma/prisma-error.ts
+++
@@ -1,4 +0,0 @@
-export enum PrismaError {
-  RecordDoesNotExist = 'P2025',
-  UniqueConstraintFailed = 'P2002',
-}
