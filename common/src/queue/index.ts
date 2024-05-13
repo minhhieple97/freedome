1de286bc626fb0e0d48e824090a75dab4fd1657d
hieple <hieplevuc@gmail.com>
2024-05-13 21:07:18 +0700

    feat: Add event http for search user and create at_rf_token

--- common/src/queue/index.ts
+++ common/src/queue/index.ts
@@ -1,9 +1,11 @@
 export const AUTH_EMAIL_QUEUE_NAME = 'auth_email_queue';
 export const ORDER_EMAIL_QUEUE_NAME = 'order_email_queue';
 export const EVENTS_RMQ = {
-  AUTH_EMAIL: 'auth_email_event',
-  USER_BUYER: 'user_buyer_event',
+  AUTH_EMAIL: 'auth_email',
+  USER_BUYER: 'user_buyer',
 };
 export const EVENTS_HTTP = {
   USER_CREATE: 'user_create',
+  USER_SEARCH_BY_CREDENTIALS: 'user_search_by_credentials',
+  AT_RF_CREATE: 'at_rf_create',
 };
