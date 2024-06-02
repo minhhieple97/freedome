b635bce97bc5000b6b15dc67b4f735614d914779
hieple <hieplevuc@gmail.com>
2024-06-02 15:33:47 +0700

    refactor: Refactor grpc method in auth service

--- microservices/auth/src/auth.controller.ts
+++ microservices/auth/src/auth.controller.ts
@@ -8,7 +8,7 @@ import {
   ResetPasswordDtoWithUserIdDto,
 } from '@freedome/common';
 import {
-  AuthServiceControllerMethods,
+  AUTH_SERVICE_NAME,
   DecodeTokenRequest,
   ForgotPasswordRequest,
   GetUserByIdRequest,
@@ -16,49 +16,61 @@ import {
   ResendEmailRequest,
   VerifyEmailRequest,
 } from 'proto/types';
+import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
-@AuthServiceControllerMethods()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @GrpcMethod(AUTH_SERVICE_NAME, 'createUser')
   public createUser(userInfo: CreateUserDto) {
     return this.authService.createUser(userInfo);
   }
 
+  @GrpcMethod(AUTH_SERVICE_NAME, 'getUserById')
   getUserById({ id }: GetUserByIdRequest) {
     return this.authService.getAuthUserById(id);
   }
 
+  @GrpcMethod(AUTH_SERVICE_NAME, 'verifyEmail')
   async verifyEmail({ token }: VerifyEmailRequest) {
     const user = await this.authService.getUserByEmailToken(token);
     return this.authService.verifyEmail(user.id, true);
   }
 
+  @GrpcMethod(AUTH_SERVICE_NAME, 'forgotPassword')
   forgotPassword({ email }: ForgotPasswordRequest) {
     return this.authService.forgotPassword(email);
   }
 
+  @GrpcMethod(AUTH_SERVICE_NAME, 'resetPassword')
   resetPassword(resetPasswordDtoWithUserId: ResetPasswordDtoWithUserIdDto) {
     return this.authService.resetPassword(resetPasswordDtoWithUserId);
   }
 
+  @GrpcMethod(AUTH_SERVICE_NAME, 'resetPasswordWithToken')
   resetPasswordWithToken(
     resetPasswordDtoWithUserId: ResetPasswordDtoWithTokenDto,
   ) {
     return this.authService.resetPasswordWithToken(resetPasswordDtoWithUserId);
   }
 
+  @GrpcMethod(AUTH_SERVICE_NAME, 'resendEmail')
   resendEmail({ email }: ResendEmailRequest) {
     return this.authService.resendEmail(email);
   }
+
+  @GrpcMethod(AUTH_SERVICE_NAME, 'getUserByCredential')
   getUserByCredential(loginUserRequest: LoginAuthRequest) {
     return this.authService.getUserByCredential(loginUserRequest);
   }
 
+  @GrpcMethod(AUTH_SERVICE_NAME, 'createToken')
   async createToken(data: IAccessTokenPayload): Promise<ITokenResponse> {
     return this.authService.createToken(data);
   }
+
+  @GrpcMethod(AUTH_SERVICE_NAME, 'decodeToken')
   public async decodeToken(
     tokenValue: DecodeTokenRequest,
   ): Promise<IAccessTokenPayload> {
