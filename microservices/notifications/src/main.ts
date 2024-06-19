396f85f7f0ca8b21c53ae57eae8869888445caf2
hieple <hieplevuc@gmail.com>
2024-06-19 21:34:19 +0700

    refactor: Remove unuse port in notification service

--- microservices/notifications/src/main.ts
+++ microservices/notifications/src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationModule } from './notification.module';
-import { AppConfigService } from './config/app/config.service';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import {
   AUTH_EMAIL_QUEUE_NAME,
   ORDER_EMAIL_QUEUE_NAME,
   LoggerService,
 } from '@freedome/common';
-import { Transport } from '@nestjs/microservices';
 import { RabbitMqService } from '@freedome/common/module';
 const logger = new LoggerService('Notifications Service');
 async function bootstrap(): Promise<void> {
@@ -17,18 +15,11 @@ async function bootstrap(): Promise<void> {
       logger,
     },
   );
-  const appConfig: AppConfigService = app.get(AppConfigService);
   const rabbitMqService = app.get(RabbitMqService);
   for (const queueName of [AUTH_EMAIL_QUEUE_NAME, ORDER_EMAIL_QUEUE_NAME]) {
     app.connectMicroservice(rabbitMqService.getRmqOptions(queueName));
   }
   await app.startAllMicroservices();
-  app
-    .connectMicroservice({
-      transport: Transport.TCP,
-      options: { host: '0.0.0.0', port: appConfig.tcpPort },
-    })
-    .listen();
 }
 bootstrap()
   .then(() => {
