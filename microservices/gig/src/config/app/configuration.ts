76f4ff2ec040e9b7d46dbcc0710cfea734bf6493
hieple <hieplevuc@gmail.com>
2024-08-03 15:12:39 +0700

    feat: Setup gig config aws

--- microservices/gig/src/config/app/configuration.ts
+++ microservices/gig/src/config/app/configuration.ts
@@ -4,4 +4,8 @@ export default registerAs('app', () => ({
   nodeEnv: process.env.NODE_ENV,
   userGrpcUrl: process.env.USER_GRPC_URL,
   mongoUri: process.env.MONGODB_URI,
+  awsAccessKey: process.env.AWS_ACCESS_KEY_ID,
+  awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
+  awsRegion: process.env.AWS_S3_REGION,
+  awsBucketS3Name: process.env.AWS_BUCKET_S3_NAME,
 }));
