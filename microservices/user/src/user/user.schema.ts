97138aa21f253b101a9c743c4e4c4639069ad1c1
hieple <hieplevuc@gmail.com>
2024-08-05 21:48:56 +0700

    feat: Setup user temporary schema

---
+++ microservices/user/src/user/user.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true })
+  userId: number;
+
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  profilePublicId: string;
+
+  @Prop({ required: true })
+  emailVerified: boolean;
+
+  @Prop({ type: Date, default: Date.now })
+  createdAt: Date;
+
+  @Prop({ type: Date, default: Date.now })
+  updatedAt: Date;
+}
+
+export type UserDocument = User & Document;
+export const UserSchema = SchemaFactory.createForClass(User);
