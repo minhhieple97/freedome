f694491b2095f3bc7ef90498844b1a4a0c093b83
hieple <hieplevuc@gmail.com>
2024-08-14 17:58:56 +0700

    update: Remove grpc connect

--- microservices/gig/src/main.ts
+++ microservices/gig/src/main.ts
@@ -3,8 +3,7 @@ import { GigModule } from './gig.module';
 import { Transport } from '@nestjs/microservices';
 
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { LoggerService, SERVICE_NAME } from '@freedome/common';
-import { join } from 'path';
+import { LoggerService } from '@freedome/common';
 import { AppConfigService } from './config/app/config.service';
 const logger = new LoggerService('Gig Service');
 async function bootstrap() {
@@ -14,16 +13,6 @@ async function bootstrap() {
   const appConfig: AppConfigService = app.get(AppConfigService);
   app.init();
   await app.startAllMicroservices();
-  await app
-    .connectMicroservice({
-      transport: Transport.GRPC,
-      options: {
-        package: SERVICE_NAME.GIG,
-        protoPath: join(__dirname, '../../../../proto/gig.proto'),
-        url: appConfig.gigGrpcUrl,
-      },
-    })
-    .listen();
   await app
     .connectMicroservice({
       transport: Transport.TCP,
